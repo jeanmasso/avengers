164bbc5937be75427a5ab98022797d8a8f3eda99
Jean Masso <68371410+jeanmasso@users.noreply.github.com>
2025-03-02 12:23:48 +1100

    AVdJ 2 - Ajout de la route /ajouter pour l'ajout de marques-pages

--- src/Controller/BookmarkController.php
+++ src/Controller/BookmarkController.php
@@ -10,8 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 
 final class BookmarkController extends AbstractController{
     #[Route('/bookmark', name: 'app_bookmark')]
-    public function index(EntityManagerInterface $entityManager)
-    {
+    public function index(EntityManagerInterface $entityManager) {
         $bookmarks = $entityManager
             ->getRepository(Bookmark::class)
             ->findAll();
@@ -19,5 +18,27 @@ final class BookmarkController extends AbstractController{
         return $this->render('bookmark/index.html.twig', [
             'bookmarks' => $bookmarks,
         ]);
+    }
+
+    #[Route("/bookmark/ajouter", name: "bookmark_ajouter")]
+    public function addBookmark(EntityManagerInterface $entityManager): Response {
+
+        $newData = [
+            ["url" => "https://symfony.com/", "comment" => "Symfony"],
+            ["url" => "https://www.qwant.com/", "comment" => "Qwant"],
+            ["url" => "https://getbootstrap.com/", "comment" => "Bootstrap"]
+        ];
+
+        foreach ($newData as $data) {
+            $bookmark = new Bookmark();
+            $bookmark->setUrl($data["url"]);
+            $bookmark->setComment($data["comment"]);
+            $bookmark->setCreatedDate(new \DateTime());
+
+            $entityManager->persist($bookmark);
+        }
+
+        $entityManager->flush();
+        return new Response("Marques-pages sauvegardés.");
     }
 }
